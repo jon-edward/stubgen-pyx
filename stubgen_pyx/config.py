910ac59252c129b7cf7ac3351a5cff61878a6f6a
jon-edward <townsend.jonathan.e@gmail.com>
2025-12-21 22:07:19 -0500

    Add stubgen config

---
+++ stubgen_pyx/config.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class StubgenPyxConfig:
+    """
+    Configuration for stubgen_pyx.
+    """
+
+    no_sort_imports: bool = False
+    no_trim_imports: bool = False
+    no_pxd_to_stubs: bool = False
+    no_normalize_names: bool = False
+    exclude_epilog: bool = False
