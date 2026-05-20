2112c2f85962252546c15a28c3ec88885d2feefe
jon-edward <townsend.jonathan.e@gmail.com>
2026-05-20 18:55:57 -0400

    Add __version__ fallback for uninstalled package usage

--- stubgen_pyx/_version.py
+++ stubgen_pyx/_version.py
@@ -1,3 +1,7 @@
 import importlib.metadata
 
-__version__: str = importlib.metadata.version(__package__)  # type: ignore
+try:
+    __version__: str = importlib.metadata.version(__package__)  # type: ignore
+except importlib.metadata.PackageNotFoundError:
+    # package is not installed
+    __version__ = "Unknown"
