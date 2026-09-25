a803bdd6655fb44a4f1d27e4a89dd1ce2da5dbe5
jon-edward <townsend.jonathan.e@gmail.com>
2026-09-24 21:33:32 -0400

    Expose conversion helpers

--- stubgen_pyx/conversion/__init__.py
+++ stubgen_pyx/conversion/__init__.py
@@ -2,18 +2,26 @@
 
 from . import (
     converter,
+    ctypedef_aliases,
+    declarations,
     docstrings,
+    fused_types,
     signature,
     source_extraction,
+    type_comments,
     type_parsing,
     unparse,
 )
 
 __all__ = [
     "converter",
+    "ctypedef_aliases",
+    "declarations",
     "docstrings",
+    "fused_types",
     "signature",
     "source_extraction",
+    "type_comments",
     "type_parsing",
     "unparse",
 ]
